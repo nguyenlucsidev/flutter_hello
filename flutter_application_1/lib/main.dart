6955734c78964405280e2a68d19d209672f8fe7b
nguyenlucsi_dev <nguyenlucsi.dev@gmail.com>
2024-12-22 17:17:17 +0700

    hello world

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,56 +1,38 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-// my app state
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
 
 
-// my home page
-class MyHomePage extends StatelessWidget  {
-
-  @override
-  Widget build(BuildContext context) {
-
-    var appState = context.watch<MyAppState>();
-    
-    // build scaffold
-    return Scaffold(
-      // body 
-      body: Column(children: [
-        Text('A random idea  2 nha'),
-        Text(appState.current.asLowerCase),
-      ],),
-    );
-  }
-  
-}
-
-
-// my app
-class MyApp extends StatelessWidget {
-
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (content) => MyAppState(),
-      child: MaterialApp(
-        title: 'Name App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)
-        ),
-
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
 
 void main() {
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp>{
+  @override
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Name here"),
+      ),
+
+     body: new Container(
+      padding: new EdgeInsets.all(32.0),
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text('Hello world')
+          ]
+        )
+      ),
+     ), 
+    );
+  }
 }
