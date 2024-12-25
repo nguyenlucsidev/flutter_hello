d714c88c1ed6b1f7bc594b4d5d420de01fd030f0
nguyenlucsi_dev <nguyenlucsi.dev@gmail.com>
2024-12-25 10:46:08 +0700

    login page

--- tutorial/tutorial/lib/main.dart
+++ tutorial/tutorial/lib/main.dart
@@ -1,51 +1,15 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-import 'Trackizer/View/app_main_screen.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Material(
+        child: Column(
+          children: [
+            Text("hello world")
+          ]
+        ),
+      ),
+    ),
+  );
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // Root widget of the app
-  @override
-  Widget build(BuildContext context) {
-    return  const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AppMainScreen(),
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for hive database
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Hive.initFlutter();
-//   await Hive.openBox("Box");
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: HiveDatabaseFlutter(),
-//     );
-//   }
-// }
