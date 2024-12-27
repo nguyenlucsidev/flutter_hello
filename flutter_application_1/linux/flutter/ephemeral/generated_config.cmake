fb62e64a910cbb4b49fb1088754820907e216c97
nguyenlucsi_dev <nguyenlucsi.dev@gmail.com>
2024-12-28 05:44:01 +0700

    sync

---
+++ flutter_application_1/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/singuyen/Documents/softwares/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/singuyen/Documents/code/flutter_hello/flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/singuyen/Documents/softwares/flutter"
+  "PROJECT_DIR=/home/singuyen/Documents/code/flutter_hello/flutter_application_1"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/singuyen/Documents/code/flutter_hello/flutter_application_1/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/singuyen/Documents/code/flutter_hello/flutter_application_1/lib/main.dart"
+)
