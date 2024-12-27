c4c7b0f40485a8d398155e0a4f5aa1a8324a8d3c
nguyenlucsi_dev <nguyenlucsi.dev@gmail.com>
2024-12-27 13:22:21 +0700

    class tutorial

--- tutorial/tutorial/lib/main.dart
+++ tutorial/tutorial/lib/main.dart
@@ -2,14 +2,25 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Material(
+          color: Colors.blueAccent,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text("hello world2 ")
+            Text("hello world2 "),
+            Text("hello again"),
+            Text("new text")
           ]
         ),
       ),
-    ),
-  );
+    );
+  }
 }
